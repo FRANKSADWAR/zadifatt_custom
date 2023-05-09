c52208e5aef6b3c786aa329f2ac22913f9b7f036
FRANKSADWAR <billyfranks98@gmail.com>
2023-05-09 22:14:25 +0300

    loan

--- zadifatt_custom/zadifatt_custom/report/loan_interests_report/loan_interests_report.js
+++ zadifatt_custom/zadifatt_custom/report/loan_interests_report/loan_interests_report.js
@@ -9,21 +9,24 @@ frappe.query_reports["Loan Interests Report"] = {
 			label: __('Applicant'),
 			fieldtype: 'Link',
 			reqd: 0,
-			width: 100
+			width: 100,
+			options: 'Customer'
 		},
 		{
-			fieldname: 'loan_application',
-			label: __('Loan Application'),
+			fieldname: 'name',
+			label: __('Loan'),
 			fieldtype: 'Link',
-			width: 100
+			width: 100,
+			options: 'Loan'
 
 		},
 		{
 			fieldname: 'status',
 			label: __('Status'),
 			fieldtype: 'Select',
-			default: 'Disbursed',
-			width: 50
+			options: ['Disbursed','Loan Closure Requested','Closed','Partially Disbursed'],
+			default: "",
+			width: 50,
 		},
 		{
 			fieldname: 'start_date',
--- zadifatt_custom/zadifatt_custom/report/loan_interests_report/loan_interests_report.py
+++ zadifatt_custom/zadifatt_custom/report/loan_interests_report/loan_interests_report.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2023, Billy Adwar && OdukTech Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
 
+import frappe
 
 def execute(filters=None):
 	columns, data = [], []
 	return columns, data
+
+def get_data(filters):
+	from_date, to_date = filters.get('from_date'),filters.get('to_date')
