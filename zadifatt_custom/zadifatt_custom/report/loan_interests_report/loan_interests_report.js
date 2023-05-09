accc9370950026b85479dc46e826f86a89396bcb
FRANKSADWAR <billyfranks98@gmail.com>
2023-05-09 21:32:12 +0300

    fetch the fields

--- zadifatt_custom/zadifatt_custom/report/loan_interests_report/loan_interests_report.js
+++ zadifatt_custom/zadifatt_custom/report/loan_interests_report/loan_interests_report.js
@@ -4,6 +4,40 @@
 
 frappe.query_reports["Loan Interests Report"] = {
 	"filters": [
+		{
+			fieldname: 'applicant',
+			label: __('Applicant'),
+			fieldtype: 'Link',
+			reqd: 0,
+			width: 100
+		},
+		{
+			fieldname: 'loan_application',
+			label: __('Loan Application'),
+			fieldtype: 'Link',
+			width: 100
 
+		},
+		{
+			fieldname: 'status',
+			label: __('Status'),
+			fieldtype: 'Select',
+			default: 'Disbursed',
+			width: 50
+		},
+		{
+			fieldname: 'start_date',
+			label: __('Start Date'),
+			fieldtype: 'Date',
+			default: frappe.datetime.add_months(frappe.datetime.get_today(),-1),
+			width: 80
+		},
+		{
+			fieldname: 'to_date',
+			label: __('To Date'),
+			fieldtype: 'Date',
+			default: frappe.datetime.get_today(),
+			width: 80
+		}
 	]
 };
