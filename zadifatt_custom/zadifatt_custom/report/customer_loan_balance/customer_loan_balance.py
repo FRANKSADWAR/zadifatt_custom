50b680010f94313376ca9413ede133c98789454b
FRANKSADWAR <billyfranks98@gmail.com>
2023-05-10 11:30:55 +0300

    new report

--- zadifatt_custom/zadifatt_custom/report/customer_loan_balance/customer_loan_balance.py
+++ zadifatt_custom/zadifatt_custom/report/customer_loan_balance/customer_loan_balance.py
@@ -1,9 +1,46 @@
 # Copyright (c) 2023, Billy Adwar && OdukTech Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
 
+import frappe
 
 def execute(filters=None):
-	columns, data = [], []
+	columns = get_columns()
+	data = get_data(filters)
 	return columns, data
+
+def get_data(filters):
+	from_date, to_date = filters.get('from_date'),filters.get('to_date')
+	conditions ="1=1"
+
+	if(filters.get('status')):
+		conditions += f" AND status='{filters.get('status')}' "
+
+	if (filters.get('name')):
+		conditions += f" AND name='{filters.get('name')}'"
+
+	if (filters.get('applicant')):
+		conditions  += f" AND applicant ='{filters.get('applicant')}'"
+
+	if filters.get('from_date') > filters.get('to_date'):
+		frappe.throw("From date cannot be greater than to date {}".format(filters.get('to_date')))
+	query = f"""
+				SELECT applicant AS Customer, sum(disbursed_amount) AS `Total Disbursed Amount`, SUM(total_payment) AS `Total Payments`, 
+				SUM(total_amount_paid) AS `Total Amount Paid`,
+        		(SUM(total_payment)-SUM(total_amount_paid)) AS `Payment Balance` FROM `tabLoan` WHERE {conditions} AND status <> 'Sanctioned' 
+				AND (disbursement_date BETWEEN '{from_date}' AND '{to_date}') GROUP BY applicant;
+				"""
+	
+	data = frappe.db.sql(query)
+
+	return data
+
+def get_columns():
+	return [
+		"Customer:Data:200",
+		"Total Disbursed Amount:Currency:200",
+		"Total Payments:Currency:200",
+		"Total Amount Paid:Currency:200",
+		"Payment Balance:Currency:200"
+	]
+
