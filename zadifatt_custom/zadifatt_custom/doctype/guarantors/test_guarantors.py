65e17931afedcb79e6c20922326d0d7f4d55638f
FRANKSADWAR <billyfranks98@gmail.com>
2023-05-10 12:03:03 +0300

    guarantors docType

---
+++ zadifatt_custom/zadifatt_custom/doctype/guarantors/test_guarantors.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Billy Adwar && OdukTech Limited and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestGuarantors(FrappeTestCase):
+	pass
